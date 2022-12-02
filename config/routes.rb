c0a36e66d8c52f5cb387bf3e7bee146a5cf231d7
amenoco <ec2-user@ip-172-31-0-176.ap-northeast-1.compute.internal>
2022-12-02 15:20:08 +0000

    controllersの飛ぶ先を考える

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'homes/top'
+  root to: 'homes#top'
+  get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: [:new, :index, :show, :edit, :destroy, :create, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+  
 end
