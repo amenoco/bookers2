f42bf3529c0976af5708740fd3460655192af4ed
amenoco <ec2-user@ip-172-31-0-176.ap-northeast-1.compute.internal>
2022-12-03 15:49:58 +0000

    ヘッターフッターした

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,14 @@
 class BooksController < ApplicationController
   
   def new
-    @books = Book.new
+   # @books = Book.new
   end
   
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path
+    # @book = Book.new(book_params)
+    # @book.user_id = current_user.id
+    # @book.save
+    # redirect_to book_path
   end
   
   def index 
