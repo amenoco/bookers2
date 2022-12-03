d678540c30c0c7e876013aa3ef6228171ff9db0e
amenoco <ec2-user@ip-172-31-0-176.ap-northeast-1.compute.internal>
2022-12-03 14:34:18 +0000

    次リンク先つなぐ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,25 @@
 class BooksController < ApplicationController
+  
+  def new
+    @books = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+  
   def index 
     @books = Book.all  
   end
+  
+  def show
+  end
+
+  def destroy
+  end
+  
   private
 end
