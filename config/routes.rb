31152235b3355e020fb3ad909fae3666200df05b
amenoco <ec2-user@ip-172-31-0-176.ap-northeast-1.compute.internal>
2022-12-06 12:20:10 +0000

    top出来た

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'homes/top'
+  
   root to: 'homes#top'
   get 'home/about'
   get "homes/about" => "homes#about", as: "about"
--- db/schema.rb
+++ db/schema.rb
@@ -48,12 +48,12 @@ ActiveRecord::Schema.define(version: 2022_12_02_113423) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
