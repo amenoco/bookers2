1b1ce19fd7970d7b94d9a17cd9b00429f49ed42f
amenoco <ec2-user@ip-172-31-0-176.ap-northeast-1.compute.internal>
2022-12-08 14:17:59 +0000

    次投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,38 @@
 class BooksController < ApplicationController
-  
+
   def new
    # @books = Book.new
   end
-  
+
+  def index
+    @books = Book.all
+    @book = Book.new
+
+  end
   def create
      @book = Book.new(book_params)
-     @book.user_id = current_user.id
+       
      @book.save
-     flash[:notice] = "Signed out successfully."
-     
+      if @book.save
+      	redirect_to book_path(book.id)
+  	  else
+		render'books/index'
+	end
+
      redirect_to book_path(book.id)
   end
-  
-  def index 
-    @books = Book.all  
-    flash[:notice] = "Signed in successfully."
-  end
-  
+
+
   def show
-       @book = Book.find(params[:id])  
+    @book = Book.find(params[:id])
+    flash[:notice] = "Signed in successfully."
   end
 
   def destroy
   end
-  
+
   private
   def book_params
-    params.require(:book).permit(:shop_name, :image, :caption)
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def show
+    @user =　Book.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+    @user =　Book.find(params[:id])
+    @books = @user.books
+  end
+
+  def destroy
+  end
+
+
 end
